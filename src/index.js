54d206af112681cd213910ad569774ee2e5c8046
Leon <Leon4w@outlook.com>
2018-02-01 13:41:19 +0800

    Use Progressive Web Application

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import _ from 'lodash';
 import printMe from './print.js';
 
+console.log('navigator: ', JSON.stringify(navigator))
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then(registration => {
+      console.log('SW registered: ', registration);
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
+
 function component() {
   var element = document.createElement('div');
   var btn = document.createElement('button');
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const webpack = require('webpack');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
   entry: {
-    app: './src/index.js'
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist',
-    hot: true
+    app: './src/index.js',
+    print: './src/print.js'
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      title: 'Output Management'
+      title: 'Progressive Web Application'
     }),
-    new webpack.NamedModulesPlugin(),
-    new webpack.HotModuleReplacementPlugin()
+    new WorkboxPlugin({
+      // 这些选项帮助 ServiceWorkers 快速启用
+      // 不允许遗留任何“旧的” ServiceWorkers
+      clientsClaim: true,
+      skipWaiting: true
+    }),
   ],
   output: {
     filename: '[name].bundle.js',
