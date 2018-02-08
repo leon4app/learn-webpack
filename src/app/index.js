405befe37da4be54a11fc57a70e32d9f9af50aa7
Leon <Leon4w@outlook.com>
2018-02-08 14:40:01 +0800

    Move print.js into src/print/

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 import './styles.css';
-// import Print from './print';
+// import Print from '../print/print';
 if (process.env.NODE_ENV !== 'production') {
   console.log('Looks like we are in development mode!');
 }
@@ -17,22 +17,22 @@ function component() {
   // element.onclick = Print.bind(null, 'Hello webpack!');
   // Note that because a network request is involved, some indication
   // of loading would need to be shown in a production-level site/app.
-  // btn.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
-  //   var print = module.default;
+  btn.onclick = e => import(/* webpackChunkName: "print" */ '../print/print').then(module => {
+    var print = module.default;
 
-  //   print('Hello webpack!');
-  // });
+    print('Hello webpack!');
+  });
   return element;
 }
 
 let element = component(); // 当 print.js 改变导致页面重新渲染时，重新获取渲染的元素
 document.body.appendChild(element);
 
-if (module.hot) {
-  module.hot.accept('./print.js', function() {
-    console.log('Accepting the updated printMe module!');
-    document.body.removeChild(element);
-    element = component(); // 重新渲染页面后，component 更新 click 事件处理
-    document.body.appendChild(element); 
-  })
-}
+// if (module.hot) {
+//   module.hot.accept('../print/print.js', function() {
+//     console.log('Accepting the updated printMe module!');
+//     document.body.removeChild(element);
+//     element = component(); // 重新渲染页面后，component 更新 click 事件处理
+//     document.body.appendChild(element); 
+//   })
+// }
